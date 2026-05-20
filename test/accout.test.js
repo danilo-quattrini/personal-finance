21c77ec1fa77ea634ad3d45e69d232d6bc467c2b
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-20 13:34:48 +0200

    test: implement test for error cases in the creation of an account object.

---
+++ test/accout.test.js
@@ -0,0 +1,24 @@
+import { assert } from 'chai';
+import {Account} from '../src/Account.js';
+
+describe('Account', () => {
+
+    describe('error cases', () => {
+        it('should throw if any account details has been provided', () => {
+            assert.throws(() => Account(null), Error);
+        });
+        it('should throw if the surname it\'s not inserted in the creation process', () => {
+            assert.throws(() => Account(`Danilo`, null, 100), Error);
+        });
+        it('should throw if the name it\'s not inserted in the creation process', () => {
+            assert.throws(() => Account(null , `Quattrini`, 100), Error);
+        });
+        it('should throw if initial balance is negative', () => {
+            assert.throws(() => Account(`Danilo`,`Quattrini`, -100), Error);
+        });
+        it('should throw if initial balance is empty', () => {
+            assert.throws(() => Account(`Danilo`,`Quattrini`, null), Error);
+        });
+    });
+
+})
