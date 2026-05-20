0df5074d9adc3d24fbc6a4e8de9f2f741b63aae9
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-20 14:00:35 +0200

    docs: JSDocs add to validation functions.

--- src/account.js
+++ src/account.js
@@ -1,9 +1,20 @@
+/**
+ * Return a number with fixed decimal approximation, throw an error
+ * if the number it is not valid
+ * @param number
+ * @returns {string}
+ */
 function numberValidation(number) {
     if(number < 0 && !isNaN(number)) {
      throw new Error(`Number must be a positive integer`);
     }
     return number.toFixed(2);
 }
+
+/**
+ * Validate if the given value it's a valid string and it's not empty
+ * @param value
+ */
 function stringValidation(value) {
     value = value.trim();
     if(value.length === 0) {
@@ -13,11 +24,12 @@ function stringValidation(value) {
         throw new Error(`Value must be a valid string`);
     }
 }
+
 export function Account(name, surname, balance) {
     stringValidation(name);
     stringValidation(surname);
     numberValidation(balance);
-    return{
+    return {
         name,
         surname,
         balance,
