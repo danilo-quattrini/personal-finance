0d451e96ae73dd4e89bfc6ce8dc934af51303279
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-21 20:39:26 +0200

    fix: remove `new` from each of the creation of a new Account in the test, that because it's not a class but a factory function, means that we don't user the new.

--- test/accout.test.js
+++ test/accout.test.js
@@ -5,28 +5,28 @@ describe('Account', () => {
 
     describe('error cases', () => {
         it('should throw if any account details has been provided', () => {
-            assert.throws(() => new Account(null), Error);
+            assert.throws(() => Account(null), Error);
         });
         it('should throw if the surname it\'s not inserted in the creation process', () => {
-            assert.throws(() => new Account(`Danilo`, null, 100), Error);
+            assert.throws(() => Account(`Danilo`, null, 100), Error);
         });
         it('should throw if the name it\'s not inserted in the creation process', () => {
-            assert.throws(() => new Account(null , `Quattrini`, 100), Error);
+            assert.throws(() => Account(null , `Quattrini`, 100), Error);
         });
         it('should throw if initial balance is negative', () => {
-            assert.throws(() => new Account(`Danilo`,`Quattrini`, -100), Error);
+            assert.throws(() => Account(`Danilo`,`Quattrini`, -100), Error);
         });
         it('should throw if initial balance is empty', () => {
-            assert.throws(() => new Account(`Danilo`,`Quattrini`, null), Error);
+            assert.throws(() => Account(`Danilo`,`Quattrini`, null), Error);
         });
         it('should throw and error if the name and surname strings are empty', () => {
-            assert.throws(() => new Account(`  `,`  `, 100), Error);
+            assert.throws(() => Account(`  `,`  `, 100), Error);
         })
         it('should throw an error if the name is empty, but the surname is filled', () => {
-            assert.throws(() => new Account(`  `,` Doe `, 100), Error);
+            assert.throws(() => Account(`  `,` Doe `, 100), Error);
         })
         it('should throw an error if the surname is empty, but the name is filled', () => {
-            assert.throws(() => new Account(` John `,`  `, 100), Error);
+            assert.throws(() => Account(` John `,`  `, 100), Error);
         })
     });
 
@@ -34,21 +34,21 @@ describe('Account', () => {
     describe('normal cases', () => {
 
         it('should return the balance', () => {
-            const account = new Account(`Danilo`, `Quattrini`, 100.99);
+            const account = Account(`Danilo`, `Quattrini`, 100.99);
             assert.equal(account.getBalance(), 100.99);
         })
         it('should return the name', () => {
-            const account = new Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 100);
             assert.equal(account.getName(), `Danilo`);
         })
 
         it('should return the surname', () => {
-            const account = new Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 100);
             assert.equal(account.getSurname(), `Quattrini`);
         })
 
         it('should return the full name', () => {
-            const account = new Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 100);
             assert.equal(account.getFullName(), `Danilo Quattrini`);
         })
 
