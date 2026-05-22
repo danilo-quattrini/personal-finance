7ee7bd8a8a85046b7b677beb7a3123d66a14ffbe
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-22 22:26:05 +0200

    feat: implement iban field in the account.js.

--- src/account.js
+++ src/account.js
@@ -1,5 +1,6 @@
 import {isNumber, isPositive, isString} from './validator/validator.js'
 import {deposit, withdraw} from './transaction.js'
+import {isIBAN} from "./validator/isIBAN.js";
 /**
  * Returns the balance of the user
  * @returns {number} the balance of the user
@@ -32,6 +33,13 @@ function getFullName() {
 }
 
 /**
+ * Returns the iban of the user
+ * @returns {string} the iban of the user
+ */
+function getIBAN() {
+    return this.iban;
+}
+/**
  * Return an array with all the transactions made by the user.
  * @returns {[]} array with a list of object that represent each transaction
  */
@@ -41,32 +49,39 @@ function getHistory() {
 
 /**
  * Function to validate the values
- * @param name
- * @param surname
- * @param balance
+ *
+ * @param {string} name
+ * @param {string} surname
+ * @param {string} iban
+ * @param {number} balance
  */
-function validate(name, surname, balance) {
+function validate(name, surname, iban, balance) {
     if (!isString(name)) {
         throw new Error(`The name you insert it's not valid`);
     }
     if (!isString(surname)) {
         throw new Error(`The name you insert it's not valid`);
     }
+    if(!isIBAN(iban)) {
+        throw new Error(`The IBAN you insert it's not a valid format`);
+    }
     if (!isNumber(balance) || !isPositive(balance)) {
         throw new Error('Invalid balance');
     }
 }
 
-export function Account(name, surname, balance) {
-    validate(name, surname, balance);
+export function Account(name, surname, iban, balance) {
+    validate(name, surname, iban, balance);
     return {
         name,
         surname,
+        iban,
         balance,
         transactionHistory: [],
         getBalance,
         getName,
         getSurname,
+        getIBAN,
         getFullName,
         getHistory,
         deposit,
