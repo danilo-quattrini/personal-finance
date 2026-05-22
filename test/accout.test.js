f9a3e20d03ccf3496e3ac92c592d23d5911a3abd
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-22 17:05:14 +0200

    test: group test into their corrective describers and add a new test of not throw an error if the balance it's 0.

--- test/accout.test.js
+++ test/accout.test.js
@@ -4,7 +4,7 @@ import {Account} from '../src/account.js';
 describe('Account', () => {
 
     describe('error cases', () => {
-        it('should throw if any account details has been provided', () => {
+        it('should throw if any account details has not been provided', () => {
             assert.throws(() => Account(null), Error);
         });
         it('should throw if the surname it\'s not inserted in the creation process', () => {
@@ -29,6 +29,37 @@ describe('Account', () => {
             assert.throws(() => Account(` John `,`  `, 100), Error);
         })
     });
+
+        describe('name & surname', () => {
+            it('should throw if the name it\'s not inserted in the creation process', () => {
+                assert.throws(() => Account(null , `Quattrini`, 100), Error);
+            });
+            it('should throw if the surname it\'s not inserted in the creation process', () => {
+                assert.throws(() => Account(`Danilo`, null, 100), Error);
+            });
+            it('should throw and error if the name and surname strings are empty', () => {
+                assert.throws(() => Account(`  `,`  `, 100), Error);
+            })
+            it('should throw an error if the name is empty, but the surname is filled', () => {
+                assert.throws(() => Account(`  `,` Doe `, 100), Error);
+            })
+            it('should throw an error if the surname is empty, but the name is filled', () => {
+                assert.throws(() => Account(` John `,`  `, 100), Error);
+            })
+        })
+
+        describe('balance', () => {
+            it('should not throw an error if the balance it\'s 0', () => {
+                assert.doesNotThrow(() => Account(`Danilo`,`Quattrini`, 0), Error);
+            });
+            it('should throw if initial balance is negative', () => {
+                assert.throws(() => Account(`Danilo`,`Quattrini`, -100), Error);
+            });
+            it('should throw if initial balance is empty', () => {
+                assert.throws(() => Account(`Danilo`,`Quattrini`, null), Error);
+            });
+        })
+      });
 
 
     describe('normal cases', () => {
