c1ca3bf28e7697714f269edcc05d14247d9a1dee
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-22 22:25:34 +0200

    test: implement test for iban field on the account.js script.

--- test/accout.test.js
+++ test/accout.test.js
@@ -11,31 +11,40 @@ describe('Account', () => {
 
         describe('name & surname', () => {
             it('should throw if the name it\'s not inserted in the creation process', () => {
-                assert.throws(() => Account(null , `Quattrini`, 100), Error);
+                assert.throws(() => Account(null , `Quattrini`,`IT60 X054 2811 1010 0000 0123 456`,100), Error);
             });
             it('should throw if the surname it\'s not inserted in the creation process', () => {
-                assert.throws(() => Account(`Danilo`, null, 100), Error);
+                assert.throws(() => Account(`Danilo`, null, `IT60 X054 2811 1010 0000 0123 456`, 100), Error);
             });
             it('should throw and error if the name and surname strings are empty', () => {
-                assert.throws(() => Account(`  `,`  `, 100), Error);
+                assert.throws(() => Account(`  `,`  `, `IT60 X054 2811 1010 0000 0123 456`,  100), Error);
             })
             it('should throw an error if the name is empty, but the surname is filled', () => {
-                assert.throws(() => Account(`  `,` Doe `, 100), Error);
+                assert.throws(() => Account(`  `,` Doe `, `IT60 X054 2811 1010 0000 0123 456`, 100), Error);
             })
             it('should throw an error if the surname is empty, but the name is filled', () => {
-                assert.throws(() => Account(` John `,`  `, 100), Error);
+                assert.throws(() => Account(` John `,`  `, `IT60 X054 2811 1010 0000 0123 456`,100), Error);
             })
         })
 
         describe('balance', () => {
             it('should not throw an error if the balance it\'s 0', () => {
-                assert.doesNotThrow(() => Account(`Danilo`,`Quattrini`, 0), Error);
+                assert.doesNotThrow(() => Account(`Danilo`,`Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 0), Error);
             });
             it('should throw if initial balance is negative', () => {
-                assert.throws(() => Account(`Danilo`,`Quattrini`, -100), Error);
+                assert.throws(() => Account(`Danilo`,`Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', -100), Error);
             });
             it('should throw if initial balance is empty', () => {
-                assert.throws(() => Account(`Danilo`,`Quattrini`, null), Error);
+                assert.throws(() => Account(`Danilo`,`Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', null), Error);
+            });
+        })
+
+        describe('iban', () => {
+            it('should return false for invalid checksum', () => {
+                assert.throws(() => Account(`Danilo`,`Quattrini`, 'IT00 X054 2811 1010 1 456', 10000), Error, `The IBAN you insert it's not a valid fo`);
+            });
+            it('should throw an error if the IBAN is in the wrong format', () => {
+                assert.throws(() => Account(`Danilo`,`Quattrini`, 'IT00 0000', 10000), Error, `The IBAN you insert it's not a valid fo`);
             });
         })
       });
@@ -43,22 +52,27 @@ describe('Account', () => {
     describe('normal cases', () => {
 
         it('should return the balance', () => {
-            const account = Account(`Danilo`, `Quattrini`, 100.99);
+            const account = Account(`Danilo`, `Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 100.99);
             assert.equal(account.getBalance(), 100.99);
         })
         it('should return the name', () => {
-            const account = Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 100);
             assert.equal(account.getName(), `Danilo`);
         })
 
         it('should return the surname', () => {
-            const account = Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 100);
             assert.equal(account.getSurname(), `Quattrini`);
         })
 
         it('should return the full name', () => {
-            const account = Account(`Danilo`, `Quattrini`, 100);
+            const account = Account(`Danilo`, `Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 100);
             assert.equal(account.getFullName(), `Danilo Quattrini`);
+        })
+
+        it('should return the IBAN', () => {
+            const account = Account(`Danilo`, `Quattrini`, 'IT60 X054 2811 1010 0000 0123 456', 100);
+            assert.equal(account.getIBAN(), 'IT60 X054 2811 1010 0000 0123 456');
         })
 
     })
