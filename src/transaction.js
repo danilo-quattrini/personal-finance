db5c9d57c0addb8184c676ff76a43dd16873e655
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-23 13:36:19 +0200

    refactor: apply strategy pattern to simplify the operation of withdraw and deposit to separate algorithms

--- src/transaction.js
+++ src/transaction.js
@@ -1,23 +1,12 @@
 import {isNumber, isPositive} from './validator/validator.js'
 import {save} from "./history.js";
-
 /**
- * Deposit an amount of money in the user account.
+ * Remove an amount of money inside the account
+ *
  * @param amount
+ * @param type
  */
-export function deposit(amount) {
-    if(! isNumber(amount) || ! isPositive(amount)) {
-        throw new Error(`The amount must valid and be a positive integer`);
-    }
-    this.balance += amount;
-    save(this.transactionHistory, {type:`deposit`, amount, balance: this.balance});
-}
-
-/**
- * Deposit an amount of money in the user account.
- * @param amount
- */
-export function withdraw(amount) {
+function debit(amount, type){
     if(! isNumber(amount) || ! isPositive(amount)) {
         throw new Error(`The amount must valid and be a positive integer`);
     }
@@ -25,5 +14,34 @@ export function withdraw(amount) {
         throw new Error(`You don't have enough balance in your account to withdraw $${amount}!`);
     }
     this.balance -= amount;
-    save(this.transactionHistory, {type:`withdraw`, amount, balance: this.balance});
+    save(this.transactionHistory, {type, amount, balance: this.balance});
+}
+
+/**
+ * Add an amount of money inside the account
+ *
+ * @param amount
+ * @param type
+ */
+function credit(amount, type){
+    if(! isNumber(amount) || ! isPositive(amount)) {
+        throw new Error(`The amount must valid and be a positive integer`);
+    }
+    this.balance += amount;
+    save(this.transactionHistory, {type, amount, balance: this.balance});
+}
+/**
+ * Deposit an amount of money in the user account.
+ * @param amount
+ */
+export function deposit(amount) {
+    credit.call(this, amount, `deposit`);
+}
+
+/**
+ * Deposit an amount of money in the user account.
+ * @param amount
+ */
+export function withdraw(amount) {
+    debit.call(this, amount, `withdraw`);
 }
