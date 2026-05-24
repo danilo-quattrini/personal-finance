3ff43045c50ab097a6deed001ecf6591c239ea52
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-24 12:59:57 +0200

    refactor: rename function from `transaction` to `operation`

--- test/history.test.js
+++ test/history.test.js
@@ -1,30 +1,30 @@
 import {assert} from 'chai';
 import {Account} from "../src/account.js";
-import {transaction} from "../src/history.js";
+import {operation} from "../src/history.js";
 
 describe("History operations", () => {
 
     describe("error cases", () => {
         it('should throw an error if the type of operation is empty', () => {
-            assert.throws(() => transaction(`  `, 100, 500), Error)
-            assert.throws(() => transaction(null, 100, 500), Error)
-            assert.throws(() => transaction(undefined, 100, 500), Error)
-            assert.throws(() => transaction(100, 500), Error)
+            assert.throws(() => operation(`  `, 100, 500), Error)
+            assert.throws(() => operation(null, 100, 500), Error)
+            assert.throws(() => operation(undefined, 100, 500), Error)
+            assert.throws(() => operation(100, 500), Error)
         })
 
-        it('should throw an error if the transaction is not a valid one', () => {
-            let transactionType = 'Not a valid transaction type name';
-            assert.throws(() => transaction(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
+        it('should throw an error if the operation is not a valid one', () => {
+            let transactionType = 'Not a valid operation type name';
+            assert.throws(() => operation(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
             transactionType = 'WithDrawD';
-            assert.throws(() => transaction(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
+            assert.throws(() => operation(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
             transactionType = '   withdraw   ';
-            assert.throws(() => transaction(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
+            assert.throws(() => operation(transactionType, 100, 500), Error, `Transaction type "${transactionType}" not found.`)
         })
 
         it('should throw an error if the amount is empty or negative', () => {
-            assert.throws(() => transaction(` Random Value `, 500), Error)
-            assert.throws(() => transaction(` Random Value `, null, 500), Error)
-            assert.throws(() => transaction(`Deposit`,-100, 500), Error)
+            assert.throws(() => operation(` Random Value `, 500), Error)
+            assert.throws(() => operation(` Random Value `, null, 500), Error)
+            assert.throws(() => operation(`Deposit`,-100, 500), Error)
         })
     })
 
