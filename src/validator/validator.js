d6edb6826ac37d448e5096154fb7767d7d2e08d5
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-27 17:50:21 +0200

    feat: implement function to validate if an account respect a set of criterias

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,3 +1,6 @@
+import { Account } from "../account.js";
+import { generateIBAN } from "../factories/IBANgenerator.js";
+
 /**
  * Check if a number is a positive integer and finite.
  * @param number
@@ -27,6 +30,14 @@ export function isNumber(number) {
 }
 
 /**
+ * Validate if the given obj it's a valid object type and it's not empty
+ *
+ * @param obj
+ */
+export function isObject(obj) {
+    return ! isEmpty(obj) && typeof obj === 'object';
+}
+/**
  * Check if a given string value respect a min and a max length
  *
  * @param value
@@ -47,6 +58,17 @@ export function isString(string) {
     return ! isEmpty(string) && typeof string === 'string';
 }
 
+/**
+ * Valide if a given value it's an account or not
+ *
+ * @param {*} value a give value to check if it's an account or not
+ * @return {true|false}
+ */
+export function isAccount(value){
+    const exampleUser = Account(`Jonathan`, `Keller`, generateIBAN("FR"), 100)
+    return   isObject(value) &&
+            (Object.keys(exampleUser).length === Object.keys(value).length);
+}
 /**
  * Check if the operation it's into a valid type
  *
