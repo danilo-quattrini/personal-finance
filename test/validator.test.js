9600d10966e9597791a61bd3afe7a7a387767f49
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-27 13:24:35 +0200

    test: implement test for validator.js script.

---
+++ test/validator.test.js
@@ -0,0 +1,43 @@
+import { assert } from 'chai'
+import { isEmpty, isNumber, isPositive } from "../src/validator/validator.js";
+describe(`Validator`, () => {
+    describe(`Numbers`, () => {
+        it(`should return false if the isNumber has empty value`, () => {
+            assert.isFalse(isNumber());
+        })
+        it(`should return false if the number is in a string format`, () => {
+            assert.isFalse(isNumber('1'));
+        })
+        it(`should not return false if the number is into a good format`, () => {
+            assert.isNotFalse(isNumber( 1));
+        })
+        it(`should return false if the number is negative`, () => {
+            assert.isFalse(isPositive(-1));
+        })
+        it(`should return false if the number is infinte`, () => {
+            assert.isFalse(isPositive(Infinity));
+        })
+        it(`should return true if the number is positive`, () => {
+            assert.isTrue(isPositive(100));
+        })
+        it(`should return true if the number is not infinite`, () => {
+            assert.isTrue(isPositive(100));
+        })
+        it(`should not return false for 0 that's a positive number`, () => {
+            assert.isNotFalse(isPositive(0));
+        })
+    })
+
+    describe(`Strings`, () => {
+        it(`should return true if the string is undefined or null`, () => {
+            let string = null;
+            assert.isTrue(isEmpty(string));
+            string = undefined;
+            assert.isTrue(isEmpty(string));
+        })
+        it(`should return false if the string is not null or undefined`, () => {
+            let string = 'random string';
+            assert.isFalse(isEmpty(string));
+        })
+    })
+})
